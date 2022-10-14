e79aa186f5c6bbba98f4843bce1a50b20af6042e
TarasZagamula <jogoogidi@gmail.com>
2022-10-15 00:45:09 +0300

    test2

--- src/components/feedback/feedback.styled.jsx
+++ src/components/feedback/feedback.styled.jsx
@@ -1,27 +1,43 @@
-import styled from '@emotion/styled'
+import styled from "@emotion/styled";
 
 export const Box = styled.div`
-display: flex;
-background-color: teal;
+font-size: 24px;
+text-align: center;
+border-radius: 8px;
+background-color: grey;
+-webkit-box-shadow: inset 6px -25px 74px 0px rgba(69,56,69,0.54);
+-moz-box-shadow: inset 6px -25px 74px 0px rgba(69,56,69,0.54);
+box-shadow: inset 6px -25px 74px 0px rgba(69,56,69,0.54);
 padding: 10px;
 margin-bottom: 10px;
 `;
 
 export const Title = styled.p`
-font-size: 12;
+font-size: 24px;
+font-weight: bold;
 `;
 
-` <div>
-<div>
-    <p>Please leave feedback</p>
-    <button type="button" onClick={this.clickGood}>good</button>
-    <button type="button" onClick={this.clickNeutral}>neutral</button>
-    <button type="button" onClick={this.clickBad}>bad</button>
-</div>
-<div>
-    <p>Statistic</p>
-    <span>good</span><span>{this.state.good}</span>
-    <span>neutral</span><span>{this.state.neutral}</span>
-    <span>bad</span><span>{this.state.bad}</span>
-</div>
-</div>`
+export const FeedbackButton = styled.button`
+border-radius: 8px;
+background-color: black;
+color: white;
+font-size: 24px;
+margin-right: 5px;
+`;
+
+export const BoxInfo = styled.div`
+display: inline;
+margin-right: 2px;
+padding: 2px;
+border-radius: 8px;
+background-color: white;
+`;
+
+export const Label = styled.span`
+font-size: 24px;
+margin-right: 2px;
+`;
+
+export const StatIndicators = styled.span`
+font-size: 24px;
+`;
